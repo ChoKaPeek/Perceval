9bc9e9fd7ffc63b7866bb7b576fdf1032cd8b4eb
Amael Tardif <amael.tardif@epita.fr>
2020-11-08 18:37:21 +0100

    tools: factorise table display into parseTable

--- tools.js
+++ tools.js
@@ -1,5 +1,24 @@
 const JOUEURS_SID = 1611105066;
 const NIVEAUX_SID = 646100848;
+const CELL_SIZE = 13;
+
+module.exports.parseTable = function (rows) {
+  const str_table = [];
+  rows.map((row) => {
+    str_line = [];
+    for (let j = 0; j < row.length; ++j) {
+      if (j && row[j - 1].length > CELL_SIZE) {
+        str_line.push(row[j].padEnd(2*CELL_SIZE - row[j - 1].length, " "));
+      }
+      else {
+        str_line.push(row[j].padEnd(CELL_SIZE, " "));
+      }
+    }
+    str_line.push("\n");
+    str_table.push(str_line.join(""));
+  });
+  return str_table;
+}
 
 module.exports.rangeDate = function (range) {
   const parts = range.split('!');
