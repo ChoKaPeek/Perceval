96d1add2c5ba526419b251ba26f289ac639f898d
Amael Tardif <amael.tardif@epita.fr>
2021-01-18 12:23:50 +0100

    nlp: first implementation

---
+++ nlp.js
@@ -0,0 +1,157 @@
+const path = require('path');
+const tf = require('@tensorflow/tfjs-node');
+const natural = require('natural');
+natural.LancasterStemmer.attach(); // str.stem() str.tokenizeAndStem()
+const tokenizer = new natural.WordTokenizer();
+
+const fs = require('fs');
+const intents = require('./intents.json');
+
+const MODEL = './model.tflearn';
+
+// promisify fs.readFile() fs.writeFile()
+fs.readFileAsync = function (filename) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, (err, buffer) => {
+            if (err) reject(err); else resolve(buffer);
+        });
+    });
+};
+
+fs.writeFileAsync = function (filename, object) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filename, object, (err, buffer) => {
+            if (err) reject(err); else resolve(buffer);
+        });
+    });
+};
+
+let saved_data = null;
+let model = null;
+
+async function initialise() {
+  saved_data = await getData();
+  model = await getModel();
+}
+
+async function createData() {
+  let words = [];
+  const labels = [];
+  const docs_x = [];
+  const docs_y = [];
+
+  const intents = await fs.readFileAsync('./intents.json')
+    .then((success) => JSON.parse(success))
+    .catch((err) => console.error(err));
+
+  if (!intents) {
+    console.error("No intention parsed");
+    return null;
+  }
+
+  intents["intents"].forEach((intent) => {
+    intent["patterns"].forEach((pattern) => {
+      const tmp_words = tokenizer.tokenize(pattern);
+      words = words.concat(tmp_words);
+      docs_x.push(tmp_words);
+      docs_y.push(intent["tag"]);
+    });
+    if (labels.find((e) => e === intent["tag"]) === undefined) {
+      labels.push(intent["tag"]);
+    }
+  });
+
+  words = [...new Set(words.map((w) => w.toLowerCase().stem()))];
+  words.sort();
+  labels.sort();
+
+  const training = [];
+  const output = [];
+
+  docs_x.forEach((doc, x) => {
+    const tmp_words = doc.map((w) => w.toLowerCase().stem());
+
+    training.push(words.map((w) => tmp_words.find(tw => tw === w) === undefined ? 0 : 1));
+
+    const output_row = new Array(labels.length).fill(0);
+    output_row[labels.indexOf(docs_y[x])] = 1;
+    output.push(output_row);
+  });
+
+  writeData({words, labels, training, output});
+
+  return {words, labels, training, output};
+}
+
+function writeData(data) {
+  const json_data = JSON.stringify(data);
+
+  return fs.writeFileAsync('./data.json', json_data)
+    .then((success) => console.log("Data stored"))
+    .catch((err) => console.error(err));
+}
+
+function getData() {
+  return fs.readFileAsync('./data.json')
+    .then((success) => JSON.parse(success))
+    .catch((err) => {
+      if (err.code === 'ENOENT') {
+        return createData();
+      }
+      console.error(err);
+      return null;
+    });
+}
+
+function getModel() {
+  return fs.access(MODEL, fs.F_OK, (err) => {
+    if (err) { // File does not exist, let's create a tf model
+      const model = tf.sequential({
+        layers: [
+          tf.layers.dense({units: 8, batchInputShape: [null, saved_data["training"][0].length]}),
+          tf.layers.dense({units: 8}),
+          tf.layers.dense({units: saved_data["output"][0].length, activation: "softmax"})
+        ]
+      });
+
+      model.compile({loss: 'meanSquaredError', optimizer: 'adam'});
+
+      return model.fit(tf.stack(saved_data["training"]), tf.stack(saved_data["output"]), {epochs: 1000, batchSize: 8})
+        .then((history) => model.save("file://" + MODEL));
+    }
+    return tf.loadLayersModel("file://" + path.join(MODEL, "model.json"));
+  });
+}
+
+function saveModel() {
+  model.save("file://" + MODEL);
+}
+
+function bag_of_words(s, words) {
+  const s_words = s.tokenizeAndStem();
+  return words.map((w) => s_words.find(sw => sw === w) === undefined ? 0 : 1);
+}
+
+async function chat(input) {
+  if (!model) {
+    await initialise();
+  }
+
+  const result = model.predict([bag_of_words(input, saved_data["words"])])[0];
+  const max_value_index = result.map((x, i) => [x, i])
+    .reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+  const tag = saved_data["labels"][max_value_index];
+  if (result[max_value_index] > 0.7) {
+    console.log("Intention detected: " + tag + " " + results[max_value_index]);
+  } else {
+    console.log("Intention detected: " + tag + " " + results[max_value_index]);
+    console.log("I didn't quite get that");
+  }
+}
+
+chat("Hi, how are you")
+//chat("Salut, comment ca va")
+//chat("I have an urge to leave")
+//chat("Okey bya")
+//chat("Let's go")
+//chat("This isn't related")
